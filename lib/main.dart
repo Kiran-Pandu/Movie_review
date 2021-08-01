dcd60f7b639292542ad188192aa3620bf773601b
haveldarx <pavan.gangadhar.2001@gmail.com>
2021-08-01 19:16:02 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/ui/home_ui.dart';
+
+void main() {
+runApp(MaterialApp(
+  home: MovieMain(),
+));
+}
+
---
+++ lib/ui/home_ui.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class MovieMain extends StatefulWidget {
+  const MovieMain({ Key? key }) : super(key: key);
+
+  @override
+  _MovieMainState createState() => _MovieMainState();
+}
+
+class _MovieMainState extends State<MovieMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello'),
+    );
+  }
+}

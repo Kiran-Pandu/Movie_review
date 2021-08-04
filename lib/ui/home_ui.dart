2387d9aab2607584b209bcbe4db483066a004667
haveldarx <pavan.gangadhar.2001@gmail.com>
2021-08-04 17:48:17 +0530

    fixed json errors

--- lib/ui/home_ui.dart
+++ lib/ui/home_ui.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:movie_app/util/network.dart';
 class MovieMain extends StatefulWidget {
-  const MovieMain({ Key? key }) : super(key: key);
-
   @override
   _MovieMainState createState() => _MovieMainState();
 }
 
 class _MovieMainState extends State<MovieMain> {
+  late Future mObject;
+  String _movieName = "Fight Club";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mObject = getMovieName(movieName: _movieName);
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Hello'),
+      child: Text(mObject.toString()),
     );
   }
+
+Future getMovieName ({required String movieName}) =>
+new Network().moviesModel(movieName: _movieName);
 }
---
+++ lib/util/api_util.dart
@@ -0,0 +1,5 @@
+
+class Util {
+  static String appId = "4f806b3c32c09329094ad84fd217f22a";
+  
+}
